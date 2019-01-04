0a56906daad5de39cb72c3e48bebae18f2c95892
Jimmy Stone <jstone05@hamline.edu>
2019-01-03 21:15:19 -0600

    updates

--- assets/app.js
+++ assets/app.js
@@ -1,52 +1,67 @@
-$(document).ready(function () {
+var buttons = ["cat", "dog", "rabbit"];
 
-    var buttons = ["cat", "dog", "rabbit"];
+function createButton() {
+
+    $("#giphySearchButtons").empty();
+
+
 
     for (var i = 0; i < buttons.length; i++) {
         var giphyBtn = $("<button>");
-        $(giphyBtn).addClass("btn");
-        $(giphyBtn).attr("data-search", buttons[i]);
-        $(giphyBtn).text(buttons[i]);
+        giphyBtn.addClass("btn mr-2 mb-2");
+        giphyBtn.attr("data-search", buttons[i]);
+        giphyBtn.text(buttons[i]);
         $("#giphySearchButtons").append(giphyBtn);
 
 
     }
+}
 
-    function displayGiphy(data) {
-        
-        $("#giphyImages").empty()
-        var dataButtonInfo = $(data).attr("data-search");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=9m05DnrzeH6yt3AWCKZi421Q74SoqIY9&q=" + data + "&limit=10&offset=1lang=en";
+function displayGiphy(data) {
 
-        console.log(queryURL);
+    $("#giphyImages").empty()
+    var dataButtonInfo = $(data).attr("data-search");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=9m05DnrzeH6yt3AWCKZi421Q74SoqIY9&q=" + data + "&limit=10&offset=1lang=en";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            giphyData = response.data;
+    console.log(queryURL);
 
-            for (var i = 0; i < giphyData.length; i++) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        giphyData = response.data;
 
-                var newDiv = $("<div>");
-                var giphyDiv = $("<img>");
-                giphyDiv.attr("src",giphyData[i].images.fixed_height.url);
-                $("#giphyImages").append(giphyDiv);
-                
-            }
+        for (var i = 0; i < giphyData.length; i++) {
 
-        });
+            var newDiv = $("<div>");
+            var giphyDiv = $("<img>");
+            giphyDiv.attr("src", giphyData[i].images.fixed_height.url);
+            giphyDiv.addClass("btn mr-1 mb-2");
+            $("#giphyImages").append(giphyDiv);
 
-    }
+        }
 
-    $(".btn").on("click", function () {
+    });
 
-        displayGiphy($(this).attr("data-search"));
-        console.log($(this).attr("data-search"));
-    })
+}
+
+$(document).on("click", ".btn", function () {
+
+    displayGiphy($(this).attr("data-search"));
+    console.log($(this).attr("data-search"));
+})
+
+$("#giphy-button").on("click", function (event) {
+    event.preventDefault();
+    var newGiphy = $("#giphy-input").val().trim();
+    buttons.push(newGiphy);
+    console.log(buttons);
+    createButton();
+})
 
 
-});
+createButton();
+
 
 
